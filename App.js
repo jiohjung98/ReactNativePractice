6821ca7e9e42163b1e3d94f9c02d4651adc7adea
jiohjung98 <ckaquz98@naver.com>
2024-02-23 17:34:14 +0900

    chore: 초기세팅 설정

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Open up App.js to start working on your apps!</Text>
+      <Image soruce={require('./dog.jpeg')} />
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -17,4 +18,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize: 16,
+    fontWeight: "bold",
+  }
 });
